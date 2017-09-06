87610b6a3e5e502e5b9917a9510e02b9f613fa09
Michael Aumock <maumock@origami3.com>
2017-09-06 09:06:59 -0600

    updated licensing

--- components/wifi/include/wifi.h
+++ components/wifi/include/wifi.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of Pako Bots Firmware.
+ * This file is part of Pako Bots division of Origami 3 Firmware.
  *
- *  Pako Bots Firmware is free software: you can redistribute it and/or modify
+ *  Pako Bots division of Origami 3 Firmware is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Pako Bots Firmware is distributed in the hope that it will be useful,
+ *  Pako Bots division of Origami 3 Firmware is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Pako Bots Firmware.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Pako Bots division of Origami 3 Firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _WIFI_H_
